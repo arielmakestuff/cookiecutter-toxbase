b04b50361972dd521306334edc1a8f658ff1fae5
Ariel De Ocampo <arielmakestuff@gmail.com>
2016-10-16 12:57:33 -0400

    Update post template generation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
+"""Post template generation hook"""
+
 import os
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
+    """Remove file from project template dir"""
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
 if __name__ == '__main__':
-    if '{{ cookiecutter.use_pypi_deployment_with_travis }}' != 'y':
-        remove_file('travis_pypi_setup.py')
-
-    if '{{ cookiecutter.create_author_file }}' != 'y':
+    if '{{ cookiecutter.create_author_file }}'.lower() != 'y':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+    if '{{ cookiecutter.use_tox }}'.lower() != 'y':
+        remove_file('tox.ini')
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
